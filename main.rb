859d98ca933e480d6ea577f35ad2a05cfe82bd8b
Dan S Turner <turner@nowwithmoredan.com>
2013-12-03 10:56:10 -0800

    Add 404, add instance variables for use in layout

--- main.rb
+++ main.rb
@@ -2,13 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
+  @title = "Home"
   erb :home
 end
 
 get '/about' do
+  @title = "about"
   erb :about
 end
 
 get '/contact' do
+  @title = "Contact"
   erb :contact
+end
+
+not_found do
+  @title = "Page Not Found"
+  erb :not_found
 end
