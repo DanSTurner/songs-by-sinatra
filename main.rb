9aa1e28b32d6cc954b191f010a1020b58d16f2d0
Dan S Turner <turner@nowwithmoredan.com>
2013-12-03 10:27:00 -0800

    Add external views, public assets

--- main.rb
+++ main.rb
@@ -12,37 +12,3 @@ end
 get '/contact' do
   erb :contact
 end
-
-__END__
-@@layout
-<% title = "Songs by Sinatra" %>
-<!doctype html>
-<html lang="en">
-<head>
-  <title><%= "#{title}" %></title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <header>
-    <h1><%= "#{title}" %></h1>
-    <nav>
-      <ul>
-        <li><a href="/" title="Home">Home</a></li>
-        <li><a href="/about" title="About">About</a></li>
-        <li><a href="/contact" title="Contact">Contact</a></li>
-      </ul>
-    </nav>
-  </header>
-  <section>
-    <%= yield %>
-  </section>
-</body>
-</html>
-@@home
-<p>Welcome to this website all about the songs by the great Frank Sinatra</p>
-
-@@about
-<p>This is a demonstration of how to create a website using Sinatra</p>
-
-@@contact
-<p>You can contact me at turner@nowwithmoredan.com</p>
