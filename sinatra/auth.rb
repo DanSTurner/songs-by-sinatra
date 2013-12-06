66f5b1a5a8048e9e850d8fc0c048cd3f89594997
Dan S Turner <turner@nowwithmoredan.com>
2013-12-05 16:16:57 -0800

    Add sinatra/extension sugar to auth

--- sinatra/auth.rb
+++ sinatra/auth.rb
@@ -1,47 +1,45 @@
 require 'sinatra/base'
 require 'sinatra/flash'
+require 'sinatra/extension'
 
-module Sinatra
-  module Auth
-    module Helpers
-      def authorized?
-        session[:admin]
-      end
-
-      def protected!
-        halt 401, slim(:unauthorized) unless authorized?
-      end
+module Auth
+  extend Sinatra::Extension
+  module Helpers
+    def authorized?
+      session[:admin]
     end
 
-    def self.registered(app)
-      app.helpers Helpers
-
-      app.enable :sessions
-
-      app.set :username => 'frank',
-              :password => 'sinatra'
-
-      app.get '/login' do
-        slim :login
-      end
-
-      app.post '/login' do
-        if params[:username] == settings.username && params[:password] == settings.password
-          session[:admin] = true
-          flash[:notice] = "Welcome #{settings.username}, you logged in successfully"
-          redirect '/songs'
-        else
-          flash[:notice] = 'Username or password is incorrect - please try again'
-          redirect '/login'
-        end
-      end
-
-      app.get '/logout' do
-        session[:admin] = nil
-        flash[:notice] = 'You are now logged out'
-        redirect '/'
-      end
+    def protected!
+      halt 401, slim(:unauthorized) unless authorized?
     end
   end
-  register Auth
+
+  helpers Helpers
+
+  enable :sessions
+
+  set :username => 'frank',
+      :password => 'sinatra'
+
+  get '/login' do
+    slim :login
+  end
+
+  post '/login' do
+    if params[:username] == settings.username && params[:password] == settings.password
+      session[:admin] = true
+      flash[:notice] = "Welcome #{settings.username}, you logged in successfully"
+      redirect '/songs'
+    else
+      flash[:notice] = 'Username or password is incorrect - please try again'
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session[:admin] = nil
+    flash[:notice] = 'You are now logged out'
+    redirect '/'
+  end
 end
+register Auth
