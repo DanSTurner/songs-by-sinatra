f5e6f5a3ef48179058eb4e217101a4f33f50dd00
Dan S Turner <turner@nowwithmoredan.com>
2013-12-10 13:42:20 -0800

    Add start_time setting, use for cache and etag

--- asset-handler.rb
+++ asset-handler.rb
@@ -7,17 +7,20 @@ class AssetHandler < Sinatra::Base
     set :cssdir, 'css'
     enable :coffeescript
     set :cssengine, 'scss'
+    set :start_time, Time.now
   end
 
   get '/javascripts/*.js' do
     pass unless settings.coffeescript?
-    last_modified File.mtime(settings.root+'/assets/'+settings.jsdir)
+    last_modified settings.start_time
+    etag settings.start_time.to_s
     cache_control :public, :must_validate
     coffee (settings.jsdir + '/' + params[:splat].first).to_sym
   end
 
   get '/*.css' do
-    last_modified File.mtime(settings.root+'/assets/'+settings.jsdir)
+    last_modified settings.start_time
+    etag settings.start_time.to_s
     cache_control :public, :must_validate
     send(settings.cssengine, (settings.cssdir + '/' + params[:splat].first).to_sym)
   end
